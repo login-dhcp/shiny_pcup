a450acaca28666c2f12bdd58a422f7e3982bd341
login-dhcp <nonamed424@gmail.com>
2022-10-12 01:00:00 +0900

    reformat, increase time dealy between each request

--- temp.js
+++ temp.js
@@ -1,351 +1,379 @@
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function range(size, startAt = 0) {
-    return [...Array(size).keys()].map(i => i + startAt);
+  return [...Array(size).keys()].map((i) => i + startAt);
 }
 
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function numberRemoveCommas(x) {
-    return x.toString().replace(/,*/g, "");
+  return x.toString().replace(/,*/g, "");
 }
 
-idolNames = ["마노", "히오리", "메구루",
-    "코가네", "마미미", "사쿠야", "유이카", "키리코",
-    "카호", "치요코", "쥬리", "린제", "나츠하",
-    "아마나", "텐카", "치유키",
-    "아사히", "후유코", "메이",
-    "토오루", "마도카", "코이토", "히나나",
-    "니치카", "미코토"
+idolNames = [
+  "마노",
+  "히오리",
+  "메구루",
+  "코가네",
+  "마미미",
+  "사쿠야",
+  "유이카",
+  "키리코",
+  "카호",
+  "치요코",
+  "쥬리",
+  "린제",
+  "나츠하",
+  "아마나",
+  "텐카",
+  "치유키",
+  "아사히",
+  "후유코",
+  "메이",
+  "토오루",
+  "마도카",
+  "코이토",
+  "히나나",
+  "니치카",
+  "미코토",
 ];
 
-waitTime = 75;
-configs = [{
-    'eventID': 40005,
-    'idols': range(23, 1),
-    'key_ranks': ["1", "10", "100", "1000", "3000"],
-    'startTime': "2020-10-12T15:00:00+09:00",
-    'endTime': "2020-10-21T12:00:00+09:00",
-},
-{
-    'eventID': 40006,
-    'idols': range(25, 1),
-    'key_ranks': ["1", "10", "100", "1000", "3000"],
-    'startTime': "2021-04-12T15:00:00+09:00",
-    'endTime': "2021-04-20T12:00:00+09:00",
-},
-{
-    'eventID': 40007,
-    'idols': range(25, 1),
-    'key_ranks': ["1", "10", "100", "1000", "3000"],
-    'startTime': "2021-10-12T16:00:00+09:00",
-    'endTime': "2021-10-18T12:00:00+09:00",
-},
-{
-    'eventID': 40008,
-    'idols': range(25, 1),
-    'key_ranks': ["1", "10", "100", "1000", "3000"],
-    'startTime': "2022-04-11T15:00:00+09:00",
-    'endTime': "2022-04-17T12:00:00+09:00",
-}]
+waitTime = 350;
+configs = [
+  {
+    eventID: 40005,
+    idols: range(23, 1),
+    key_ranks: ["1", "10", "100", "1000", "3000"],
+    startTime: "2020-10-12T15:00:00+09:00",
+    endTime: "2020-10-21T12:00:00+09:00",
+  },
+  {
+    eventID: 40006,
+    idols: range(25, 1),
+    key_ranks: ["1", "10", "100", "1000", "3000"],
+    startTime: "2021-04-12T15:00:00+09:00",
+    endTime: "2021-04-20T12:00:00+09:00",
+  },
+  {
+    eventID: 40007,
+    idols: range(25, 1),
+    key_ranks: ["1", "10", "100", "1000", "3000"],
+    startTime: "2021-10-12T16:00:00+09:00",
+    endTime: "2021-10-18T12:00:00+09:00",
+  },
+  {
+    eventID: 40008,
+    idols: range(25, 1),
+    key_ranks: ["1", "10", "100", "1000", "3000"],
+    startTime: "2022-04-11T15:00:00+09:00",
+    endTime: "2022-04-17T12:00:00+09:00",
+  },
+];
 
 data_all = {};
 config = configs[configs.length - 1];
 
 $(document).ready(function (e) {
-    init();
-})
+  init();
+});
 
 async function init() {
-    buildEventIDSelector();
+  buildEventIDSelector();
 
-    await getAllData(waitTime);
-    // await sleep(waitTime * (num_idols) + 5000);
-    await sleep(1000);
+  await getAllData(waitTime);
+  // await sleep(waitTime * (num_idols) + 5000);
+  await sleep(1000);
 
-    buildTimeSlider();
+  buildTimeSlider();
 
-    await update();
-    alert('finished!');
+  await update();
+  alert("finished!");
 }
 
 function updateHTML(time) {
-    buildBasicTable(time);
-    buildPredictionTable(time);
-    buildRankingTable(time);
+  buildBasicTable(time);
+  buildPredictionTable(time);
+  buildRankingTable(time);
 }
 
 async function update() {
-    var slider = document.getElementById("timeRange");
-    var key_ranks = config['key_ranks'];
+  var slider = document.getElementById("timeRange");
+  var key_ranks = config["key_ranks"];
 
-    var sample_data = data_all[0][key_ranks[0]];
-    var time = sample_data[slider.value - 1]['summaryTime'];
-    setTimeText(time);
+  var sample_data = data_all[0][key_ranks[0]];
+  var time = sample_data[slider.value - 1]["summaryTime"];
+  setTimeText(time);
 
-    updateHTML(slider.value - 1);
+  updateHTML(slider.value - 1);
 }
 
 async function getAllData(waitTime) {
-    var idols = config['idols'];
-    for (let i = 0; i < idols.length; i++) {
-        await sleep(waitTime);
-        var key_ranks_str = config['key_ranks'].join(',');
-        getDataAPI(config['eventID'], i + 1, key_ranks_str);
-    }
+  var idols = config["idols"];
+  for (let i = 0; i < idols.length; i++) {
+    await sleep(waitTime);
+    var key_ranks_str = config["key_ranks"].join(",");
+    getDataAPI(config["eventID"], i + 1, key_ranks_str);
+  }
 }
 
 function getDataAPI(eventId, characterId, ranks) {
-    var xhttp = new XMLHttpRequest();
-    var returnData = {};
-    xhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            var values = JSON.parse(this.responseText);
-            for (let i = 0; i < values.length; i++) {
-                var value = values[i];
-                returnData[value['rank']] = value['data'];
-            }
-        };
-    };
+  var xhttp = new XMLHttpRequest();
+  var returnData = {};
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      var values = JSON.parse(this.responseText);
+      for (let i = 0; i < values.length; i++) {
+        var value = values[i];
+        returnData[value["rank"]] = value["data"];
+      }
+    }
+  };
 
-    var url = `https://api.matsurihi.me/sc/v1/events/fanRanking/${eventId}/rankings/logs/${characterId}/${ranks}`;
-    xhttp.open("GET", url, false);
-    xhttp.setRequestHeader("Content-type", "text/plain");
-    xhttp.send();
+  var url = `https://api.matsurihi.me/sc/v1/events/fanRanking/${eventId}/rankings/logs/${characterId}/${ranks}`;
+  xhttp.open("GET", url, false);
+  xhttp.setRequestHeader("Content-type", "text/plain");
+  xhttp.send();
 
-    data_all[characterId - 1] = returnData;
-    return returnData;
-};
+  data_all[characterId - 1] = returnData;
+  return returnData;
+}
 
 ///////////////////////
 
 function buildEventIDSelector() {
-    var select = document.getElementById('eventID_select');
-    for (let i = 0; i < configs.length; i++) {
-        var _config = configs[i];
-        var _eventID = _config['eventID'];
+  var select = document.getElementById("eventID_select");
+  for (let i = 0; i < configs.length; i++) {
+    var _config = configs[i];
+    var _eventID = _config["eventID"];
 
-        var opt = document.createElement('option');
-        opt.value = _eventID;
-        opt.innerHTML = `${(_eventID - 40000) / 2.}주년`;
-        select.appendChild(opt);
-    }
-    select.selectedIndex = configs.length - 1;
+    var opt = document.createElement("option");
+    opt.value = _eventID;
+    opt.innerHTML = `${(_eventID - 40000) / 2}주년`;
+    select.appendChild(opt);
+  }
+  select.selectedIndex = configs.length - 1;
 
-    document.getElementById('eventID_select_button').addEventListener('click', async function (e) {
-        var _config_index = document.getElementById('eventID_select').selectedIndex;
-        config = configs[_config_index];
-        await getAllData(waitTime);
-        update();
-        alert('finished!');
+  document
+    .getElementById("eventID_select_button")
+    .addEventListener("click", async function (e) {
+      var _config_index =
+        document.getElementById("eventID_select").selectedIndex;
+      config = configs[_config_index];
+      await getAllData(waitTime);
+      update();
+      alert("finished!");
     });
 }
 
 function buildBasicTable(time) {
-    var code = '';
-    code += '<tr>\n';
-    code += '<th>Idol</th>\n';
+  var code = "";
+  code += "<tr>\n";
+  code += "<th>Idol</th>\n";
 
-    var key_ranks = config['key_ranks'];
-    var idols = config['idols'];
+  var key_ranks = config["key_ranks"];
+  var idols = config["idols"];
 
-    // 1. 각 열 추가
+  // 1. 각 열 추가
+  for (let j = 0; j < key_ranks.length; j++) {
+    code += `<th>${key_ranks[j]}</th>\n`;
+  }
+  code += "</tr>\n";
+
+  // 2. 각 데이터 추가
+  for (let i = 0; i < idols.length; i++) {
+    code += "<tr>\n";
+    code += `<td>${idolNames[i]}</td>\n`;
+
+    // 2. 아이돌별 데이터 추가
+    var data_idol = data_all[i];
     for (let j = 0; j < key_ranks.length; j++) {
-        code += `<th>${key_ranks[j]}</th>\n`;
-    }
-    code += '</tr>\n';
+      var rank = key_ranks[j];
+      var data_rank = data_idol[rank];
+      var data_final_log;
 
-    // 2. 각 데이터 추가
-    for (let i = 0; i < idols.length; i++) {
-        code += '<tr>\n';
-        code += `<td>${idolNames[i]}</td>\n`
+      var sample_data = data_all[0][key_ranks[0]];
+      var numTimes = sample_data.length;
 
-        // 2. 아이돌별 데이터 추가
-        var data_idol = data_all[i];
-        for (let j = 0; j < key_ranks.length; j++) {
-            var rank = key_ranks[j];
-            var data_rank = data_idol[rank];
-            var data_final_log;
-
-            var sample_data = data_all[0][key_ranks[0]];
-            var numTimes = sample_data.length;
-
-            // 2.1. 시간대별 데이터 추가
-            // time이 -1일 경우 최종데이터
-            // time이 index로 주어질 경우 실제 시간 계산해서 사용 TODO
-            if (time === -1) {
-                data_final_log = data_rank[data_rank.length - 1];
-            } else {
-                if (data_rank.length < numTimes) {
-                    var realTime = time - (numTimes - data_rank.length);
-                    if (realTime < 0) {
-                        data_final_log = data_rank[0];
-                    } else {
-                        data_final_log = data_rank[realTime];
-                    }
-                } else {
-                    data_final_log = data_rank[time];
-                }
-            }
-            code += `<td>${numberWithCommas(data_final_log["score"])}</td>\n`;
+      // 2.1. 시간대별 데이터 추가
+      // time이 -1일 경우 최종데이터
+      // time이 index로 주어질 경우 실제 시간 계산해서 사용 TODO
+      if (time === -1) {
+        data_final_log = data_rank[data_rank.length - 1];
+      } else {
+        if (data_rank.length < numTimes) {
+          var realTime = time - (numTimes - data_rank.length);
+          if (realTime < 0) {
+            data_final_log = data_rank[0];
+          } else {
+            data_final_log = data_rank[realTime];
+          }
+        } else {
+          data_final_log = data_rank[time];
         }
-        code += '</tr>\n';
+      }
+      code += `<td>${numberWithCommas(data_final_log["score"])}</td>\n`;
     }
-    code += '</table>\n';
+    code += "</tr>\n";
+  }
+  code += "</table>\n";
 
-    document.getElementById("main_table").innerHTML = code;
-};
+  document.getElementById("main_table").innerHTML = code;
+}
 
 function buildTimeSlider() {
-    var slider = document.getElementById("timeRange");
+  var slider = document.getElementById("timeRange");
 
-    var key_ranks = config['key_ranks'];
-    var sample_data = data_all[0][key_ranks[0]];
+  var key_ranks = config["key_ranks"];
+  var sample_data = data_all[0][key_ranks[0]];
 
-    slider.max = sample_data.length;
-    slider.value = sample_data.length;
+  slider.max = sample_data.length;
+  slider.value = sample_data.length;
 
-    var time = sample_data[slider.value - 1]['summaryTime'];
-    setTimeText(time);
+  var time = sample_data[slider.value - 1]["summaryTime"];
+  setTimeText(time);
 
-    slider.addEventListener("input", function () {
-        update();
-    });
+  slider.addEventListener("input", function () {
+    update();
+  });
 }
 
 function buildPredictionTable(time) {
-    var code = '';
-    code += '<tr>\n';
-    code += '<th>Idol</th>\n';
+  var code = "";
+  code += "<tr>\n";
+  code += "<th>Idol</th>\n";
 
-    var key_ranks = config['key_ranks'];
-    var idols = config['idols'];
+  var key_ranks = config["key_ranks"];
+  var idols = config["idols"];
+  for (let j = 0; j < key_ranks.length; j++) {
+    code += `<th>${key_ranks[j]}</th>\n`;
+  }
+  code += "</tr>\n";
+
+  for (let i = 0; i < idols.length; i++) {
+    code += "<tr>\n";
+    code += `<td>${idolNames[i]}</td>\n`;
+
+    var data_idol = data_all[i];
     for (let j = 0; j < key_ranks.length; j++) {
-        code += `<th>${key_ranks[j]}</th>\n`;
-    }
-    code += '</tr>\n';
+      var rank = key_ranks[j];
+      var data_rank = data_idol[rank];
+      var data_final_log;
 
-    for (let i = 0; i < idols.length; i++) {
-        code += '<tr>\n';
-        code += `<td>${idolNames[i]}</td>\n`
-
-        var data_idol = data_all[i];
-        for (let j = 0; j < key_ranks.length; j++) {
-            var rank = key_ranks[j];
-            var data_rank = data_idol[rank];
-            var data_final_log;
-
-            var sample_data = data_all[0][key_ranks[0]];
-            var numTimes = sample_data.length;
-            if (time === -1) {
-                data_final_log = data_rank[data_rank.length - 1];
-            } else {
-                if (data_rank.length < numTimes) {
-                    var realTime = time - (numTimes - data_rank.length);
-                    if (realTime < 0) {
-                        data_final_log = data_rank[0];
-                    } else {
-                        data_final_log = data_rank[realTime];
-                    }
-                } else {
-                    data_final_log = data_rank[time];
-                }
-            }
-            var score = data_final_log["score"];
-            var timeString = data_final_log["summaryTime"];
-            var startTime = config['startTime'];
-            var endTime = config['endTime'];
-            var totalTime = timeDiff(startTime, endTime);
-            var currentTime = timeDiff(startTime, timeString);
-            if (currentTime < totalTime) {
-                score = score * totalTime / currentTime;
-            }
-            code += `<td>${numberWithCommas(parseInt(score))}</td>\n`;
+      var sample_data = data_all[0][key_ranks[0]];
+      var numTimes = sample_data.length;
+      if (time === -1) {
+        data_final_log = data_rank[data_rank.length - 1];
+      } else {
+        if (data_rank.length < numTimes) {
+          var realTime = time - (numTimes - data_rank.length);
+          if (realTime < 0) {
+            data_final_log = data_rank[0];
+          } else {
+            data_final_log = data_rank[realTime];
+          }
+        } else {
+          data_final_log = data_rank[time];
         }
-        code += '</tr>\n';
+      }
+      var score = data_final_log["score"];
+      var timeString = data_final_log["summaryTime"];
+      var startTime = config["startTime"];
+      var endTime = config["endTime"];
+      var totalTime = timeDiff(startTime, endTime);
+      var currentTime = timeDiff(startTime, timeString);
+      if (currentTime < totalTime) {
+        score = (score * totalTime) / currentTime;
+      }
+      code += `<td>${numberWithCommas(parseInt(score))}</td>\n`;
     }
-    code += '</table>\n';
+    code += "</tr>\n";
+  }
+  code += "</table>\n";
 
-    document.getElementById("predict_table").innerHTML = code;
-};
+  document.getElementById("predict_table").innerHTML = code;
+}
 
 function buildRankingTable(time) {
-    var code = '';
-    code += '<tr>\n';
-    code += '<th>Idol</th>\n';
-    var key_ranks = config['key_ranks'];
-    var idols = config['idols'];
+  var code = "";
+  code += "<tr>\n";
+  code += "<th>Idol</th>\n";
+  var key_ranks = config["key_ranks"];
+  var idols = config["idols"];
+  for (let j = 0; j < key_ranks.length; j++) {
+    code += `<th>${key_ranks[j]}</th>\n`;
+  }
+  code += "</tr>\n";
+
+  var table_target = document.getElementById("main_table");
+  for (let i = 0; i < idols.length; i++) {
+    code += "<tr>\n";
+    code += `<td>${idolNames[i]}</td>\n`;
+
+    var data_idol = data_all[i];
     for (let j = 0; j < key_ranks.length; j++) {
-        code += `<th>${key_ranks[j]}</th>\n`;
+      var column_values = [];
+      for (let k = 0; k < idols.length; k++) {
+        var score = table_target.rows[k + 1].cells[j + 1].innerHTML;
+        score = numberRemoveCommas(score);
+        score = Number(score);
+        column_values.push(score);
+      }
+      var sorted_values = column_values.slice();
+      sorted_values.sort(function (a, b) {
+        return a - b;
+      });
+      code += `<td>${numberWithCommas(
+        sorted_values.length - sorted_values.indexOf(column_values[i])
+      )}</td>\n`;
     }
-    code += '</tr>\n';
+    code += "</tr>\n";
+  }
+  code += "</table>\n";
 
-    var table_target = document.getElementById('main_table');
-    for (let i = 0; i < idols.length; i++) {
-        code += '<tr>\n';
-        code += `<td>${idolNames[i]}</td>\n`
-
-        var data_idol = data_all[i];
-        for (let j = 0; j < key_ranks.length; j++) {
-            var column_values = [];
-            for (let k = 0; k < idols.length; k++) {
-                var score = table_target.rows[k + 1].cells[j + 1].innerHTML;
-                score = numberRemoveCommas(score);
-                score = Number(score);
-                column_values.push(score);
-            }
-            var sorted_values = column_values.slice();
-            sorted_values.sort(function (a, b) { return a - b; });
-            code += `<td>${numberWithCommas(sorted_values.length - sorted_values.indexOf(column_values[i]))}</td>\n`;
-        }
-        code += '</tr>\n';
-    }
-    code += '</table>\n';
-
-    document.getElementById("ranking_table").innerHTML = code;
-};
+  document.getElementById("ranking_table").innerHTML = code;
+}
 
 ///////////////////////
 
 function parseTime(time) {
-    // example data: "2021-04-20T15:00:00+09:00"
-    var values = {};
-    values['YYYY'] = time.slice(0, 4);
-    values['MM'] = time.slice(5, 7);
-    values['DD'] = time.slice(8, 10);
+  // example data: "2021-04-20T15:00:00+09:00"
+  var values = {};
+  values["YYYY"] = time.slice(0, 4);
+  values["MM"] = time.slice(5, 7);
+  values["DD"] = time.slice(8, 10);
 
-    values['hh'] = time.slice(11, 13);
-    values['mm'] = time.slice(14, 16);
-    values['ss'] = time.slice(17, 19);
+  values["hh"] = time.slice(11, 13);
+  values["mm"] = time.slice(14, 16);
+  values["ss"] = time.slice(17, 19);
 
-    values['utc'] = time.slice(19, 20);
-    values['utc_hh'] = time.slice(20, 22);
-    values['utc_mm'] = time.slice(24, 26);
+  values["utc"] = time.slice(19, 20);
+  values["utc_hh"] = time.slice(20, 22);
+  values["utc_mm"] = time.slice(24, 26);
 
-    return values;
+  return values;
 }
 
 function timeDiff(start, end) {
-    // time2 > time1
-    // startTime="2021-04-20T15:00:00+09:00";
-    // endTime="2021-04-20T12:00:00+09:00";
+  // time2 > time1
+  // startTime="2021-04-20T15:00:00+09:00";
+  // endTime="2021-04-20T12:00:00+09:00";
 
-    // TODO need to change this code in case of event longer than a month
-    // TODO if different utc is given...
-    var parsedStart = parseTime(start);
-    var parsedCurrnet = parseTime(end);
+  // TODO need to change this code in case of event longer than a month
+  // TODO if different utc is given...
+  var parsedStart = parseTime(start);
+  var parsedCurrnet = parseTime(end);
 
-    var diffHour = Number(parsedCurrnet['DD'] - parsedStart['DD']) * 24 +
-        Number(parsedCurrnet['hh'] - parsedStart['hh']) +
-        Number(parsedCurrnet['mm'] - parsedStart['mm']) / 60;
-    return diffHour;
-
+  var diffHour =
+    Number(parsedCurrnet["DD"] - parsedStart["DD"]) * 24 +
+    Number(parsedCurrnet["hh"] - parsedStart["hh"]) +
+    Number(parsedCurrnet["mm"] - parsedStart["mm"]) / 60;
+  return diffHour;
 }
 
 function setTimeText(time) {
-    document.getElementById("timeText").innerHTML = `기록 시간: ${time}`;
+  document.getElementById("timeText").innerHTML = `기록 시간: ${time}`;
 }
