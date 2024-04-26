05dc61171027ee5ee938f5a2ade28e8e2fec26b7
login-dhcp <nonamed424@gmail.com>
2024-04-26 19:04:00 +0900

    add missing parenthesis

--- temp.js
+++ temp.js
@@ -254,7 +254,7 @@ function buildBasicTable(time) {
         code += `<td>${numberWithCommas(data_final_log["score"])}</td>\n`;
       }
       else {
-        code += `<td>${numberWithCommas(1)</td>\n`;
+        code += `<td>${numberWithCommas(1)}</td>\n`;
       }
     }
     code += "</tr>\n";
