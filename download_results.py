f3ef13fe22b9b96f418368c0a99a0f2210a569ab
login-dhcp <nonamed424@gmail.com>
2025-04-13 17:38:55 +0900

    support more sources

--- download_results.py
+++ download_results.py
@@ -2,21 +2,30 @@
 Simple script to download results and use them as a cache.
 """
 import argparse
+import enum
 import json
 import logging
 import os
 import requests
 import time
 
+class DataSource(enum.Enum):
+  MATSURIHIME = 0
+  CHIYOKO = 1
+
+
 EVENT_IDS = list(range(40007, 40014+1))
 CHARACTER_IDS = list(range(1, 28+1))  # TODO old events don't have 28 chars
 # KEY_RANKS = list(range(1, 11)) + [10, 100, 1000, 3000]
 KEY_RANKS = [1, 10, 100, 1000, 3000]
-uri_format = "https://api.matsurihi.me/sc/v1/events/fanRanking/{eventId}/rankings/logs/{characterId}/{ranks}"
+uri_formats = {
+  DataSource.MATSURIHIME.name: "https://api.matsurihi.me/sc/v1/events/fanRanking/{eventId}/rankings/logs/{characterId}/{ranks}",
+  DataSource.CHIYOKO.name: "https://api.chiyoko.cc/sc/v1/events/fanRanking/{eventId}/rankings/logs/{characterId}/{ranks}",
+}
 
 
-def fetch_and_save(event_id: int, character_id: int, ranks: str):
-  uri = uri_format.format(
+def fetch_and_save(data_source: str, event_id: int, character_id: int, ranks: str):
+  uri = uri_formats[data_source].format(
     eventId=event_id,
     characterId=character_id,
     ranks=ranks
@@ -39,13 +48,14 @@ def main(args):
   event_id = args.event_id
   for character_id in CHARACTER_IDS:
     ranks = ",".join(map(str, KEY_RANKS))
-    fetch_and_save(event_id, character_id, ranks)
+    fetch_and_save(args.data_source, event_id, character_id, ranks)
     time.sleep(1)
 
 
 def parse_args():
   parser = argparse.ArgumentParser()
   parser.add_argument('--event_id', type=int, required=True)
+  parser.add_argument('--data_source', type=str, default=DataSource.MATSURIHIME.name)
 
   args = parser.parse_args()
   return args
