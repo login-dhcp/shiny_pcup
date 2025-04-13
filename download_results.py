ae017384afaaef0fd19048a0f00ee3ab6626928d
login-dhcp <nonamed424@gmail.com>
2025-04-13 15:33:57 +0900

    add script to download results

---
+++ download_results.py
@@ -0,0 +1,46 @@
+"""
+Simple script to download results and use them as a cache.
+"""
+import json
+import logging
+import os
+import requests
+import time
+
+EVENT_IDS = list(range(40007, 40014+1))
+CHARACTER_IDS = list(range(1, 28+1))  # TODO old events don't have 28 chars
+KEY_RANKS = list(range(1, 11)) + [10, 100, 1000, 3000]
+uri_format = "https://api.matsurihi.me/sc/v1/events/fanRanking/{eventId}/rankings/logs/{characterId}/{ranks}"
+
+
+def fetch_and_save(event_id: int, character_id: int, rank: int):
+  uri = uri_format.format(
+    eventId=event_id,
+    characterId=character_id,
+    ranks=rank
+  )
+  logging.info(uri)
+
+  output_path = f"./results/{event_id}/{character_id}/{rank}.json"
+  os.makedirs(os.path.dirname(output_path), exist_ok=True)
+  try:
+    data = requests.get(uri).text
+    data_json_format = json.loads(data)
+    with open(output_path, 'w', encoding='utf-8') as f:
+      json.dump(data_json_format, f, indent=4)
+
+  except Exception as e:
+    logging.error(f"error {e} on get {uri}")
+
+
+def main():
+  for event_id in EVENT_IDS:
+    for character_id in CHARACTER_IDS:
+      for rank in KEY_RANKS:
+        fetch_and_save(event_id, character_id, rank)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO)
+  main()
